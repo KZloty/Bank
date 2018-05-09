03e1952ecf0d2645ccd4bc2fc6435b3553e69d63
krzysiek <zlotkowski.slask@gmail.com>
2018-05-09 11:42:19 +0200

    podstawowe dane klienta

---
+++ Bank/Bank.Domain/CustomerConntact/Phone.cs
@@ -0,0 +1,15 @@
+﻿namespace Bank.Domain
+{
+    public class Phone
+    {
+        public int PhoneID { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+
+        public int CustomerID { get; set; }
+
+        public Customer Customer { get; set; }
+    }
+}
